8a98c441d6d06d6447a1bf27a7e1ba0107a2cc5a
gdjuarez <33164842+gdjuarez@users.noreply.github.com>
2023-03-20 15:22:34 -0300

    Update login.php

--- conexion/login.php
+++ conexion/login.php
@@ -55,35 +55,43 @@ $pass = strip_tags(sha1($_POST['pass']));
 
 	if($user==$elemento['user'] && $pass==$elemento['pass']){
 	
-		echo 'user:'.$elemento['user'].' pass: '.$elemento['pass'];
+		//echo 'user:'.$elemento['user'].' pass: '.$elemento['pass'];
+		$roll_numero=$elemento['roll'];
+
+		$_SESSION['logged'] = 'yes';
+		$_SESSION['user'] = $user;
+		$_SESSION['roll'] = $roll_numero;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+		switch ($roll_numero) {
 		
-	}else{
-		echo 'no corresponde';
-	}
+			case 1:
+				//	1 Admin
+				echo '<script>window.location="../prestamos/menu.php"</script>';
+				break;
+			case 2:
+				//2 EMATP - BIBLIOTECARIO
+				echo '<script>window.location="../prestamos/menu.php"</script>';
+				break;
+			case 4:
+				//4 Profesor
+				echo '<script>window.location="../reservas/reservas.php"</script>';
+				break;
+		}
+			
+			//echo '<script language=javascript>
+			// alert("Logeado exitosamente ,Bienvenido!!")</script>';
+			//echo '<script>window.location="prestamos/menu.php"</script>';
+			
+	  }else{
+		  
+		echo '<script language=javascript>
+			  alert("Usuario y/o clave, son incorrectos")
+			  self.location = "../index.php"</script>';										
+	  }
+	
 
-	echo "<br>";
-    echo"<table><tr><td>";
-    echo $elemento['id']."</td><td>";
-    echo $elemento['user']."</td><td>";
-    echo $elemento['pass']."</td><td>";
-    echo $elemento['apellido_nombre']."</td><td>";
-    echo $elemento['roll']."</td><td></<tr> </table>";
-    echo "<br>";
    
 
 
