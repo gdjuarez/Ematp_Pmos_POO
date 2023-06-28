4d13a80d4dedbb973225e2778bcf56592d035118
gdjuarez <33164842+gdjuarez@users.noreply.github.com>
2023-06-28 15:57:29 -0300

    Update config.php

--- conexion/config.php
+++ conexion/config.php
@@ -1,11 +1,18 @@
 <?php
  
-    // constantes de conexion
+    // constantes de conexion local
     define('DB_HOST','localhost');   
     define('DB_USUARIO','root');
     define('DB_PASS','');  
     define('DB_NOMBRE','u345632271_pmo'); 
     define('DB_CHARSET','utf8');
+
+    // constantes de conexion hostinger
+    // define('DB_HOST','localhost');   
+    // define('DB_USUARIO','root');
+    // define('DB_PASS','');  
+    // define('DB_NOMBRE','u345632271_pmo'); 
+    // define('DB_CHARSET','utf8');
 
  
 ?>
