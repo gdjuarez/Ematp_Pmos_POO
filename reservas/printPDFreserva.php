165d6ff1f18208d6e4e1525d8d043a398e84164d
gdjuarez <33164842+gdjuarez@users.noreply.github.com>
2023-08-22 08:14:55 -0300

    Update printPDFreserva.php

--- reservas/printPDFreserva.php
+++ reservas/printPDFreserva.php
@@ -28,23 +28,6 @@ if(isset($_POST["fechareserva"])){
     $dia=date('%d/%m/%Y');
 }
 
-
-//Obtengo datos del pedido
-$reserva = mysqli_query($miConexion,"SELECT id, dispositivo_id, Dispositivo, date_format(fecha,'%d/%m/%Y') as Fecha,Turno,Hora, Curso, Profesor, usuario FROM reserva where Fecha = '$dia' order by Fecha,Turno,Hora ");
-$item = 0;
-while($fila = mysqli_fetch_array($reserva)){
-	$item = $item+1;
-    $id= $fila['id'];  
-    $Dispositivo= $fila['Dispositivo']; 
-    $Fecha= $fila['Fecha'];
-    $Turno= $fila['Turno'];
-    $Hora= $fila['Hora'];
-    $Curso= $fila['Curso'];
-    $Profesor= $fila['Profesor'];
-   
-    
-}
-
 //-----------------------------------------------------------------------------------------------
 //Inicio la construccion del PDF
 define('PESOS',chr(128)); // Constante con el símbolo Euro.
@@ -58,24 +41,46 @@ $pdf->SetFont('Helvetica','',8);
  
 // DATOS FACTURA        
 $pdf->Ln(5);
-$pdf->Cell(60,4,'Reserva n : '.$id.'',0,1,'');
-$pdf->Cell(60,4,'Fecha: '.$Fecha.'',0,1,'');
+//$pdf->Cell(60,4,'Reserva n : '.$id.'',0,1,'');
+$pdf->Cell(60,4,'Fecha: '.$dia.'',0,1,'');
 //$pdf->Cell(60,4,'Metodo de pago: Tarjeta',0,1,'');
+ // linea
+ $pdf->Ln(4);
+ $pdf->Cell(60,0,'','T');
+ $pdf->Ln(2);    
+
+//Obtengo datos del pedido
+$reserva = mysqli_query($miConexion,"SELECT id, dispositivo_id, Dispositivo, date_format(fecha,'%d/%m/%Y') as Fecha,
+Turno,Hora, Curso, Profesor, usuario FROM reserva where Fecha = '$dia' order by Fecha,Turno,Hora ");
+
+$item = 0;
+while($fila = mysqli_fetch_array($reserva)){
+	$item = $item+1;
+    $id= $fila['id'];  
+    $Dispositivo= $fila['Dispositivo']; 
+    $Fecha= $fila['Fecha'];
+    $Turno= $fila['Turno'];
+    $Hora= $fila['Hora'];
+    $Curso= $fila['Curso'];
+    $Profesor= $fila['Profesor'];     
+    
+    // COLUMNAS
+    $pdf->SetFont('Helvetica', 'B', 7);
+    $pdf->Cell(30, 10,$Dispositivo, 0);
+    $pdf->Cell(3, 10, 'Turno: '.$Turno.'',0,0,'R');
+    $pdf->Cell(12, 10,'Hora: '.$Hora.'',0,0,'R');
+    $pdf->Cell(15, 10,'Curso: '.$Curso.'',0,0,'R');
+    $pdf->Ln(8);
+   // $pdf->Cell(60,0,'','T');
+    $pdf->Ln(0);
+    $pdf->Cell(25, 10, 'Prof: '.$Profesor.'', 0);   
+    // linea
+    $pdf->Ln(8);
+    $pdf->Cell(60,0,'','T');
+    $pdf->Ln(2);    
+    
+}
  
-// COLUMNAS
-$pdf->SetFont('Helvetica', 'B', 7);
-$pdf->Cell(30, 10,$Dispositivo, 0);
-$pdf->Cell(3, 10, 'Turno: '.$Turno.'',0,0,'R');
-$pdf->Cell(12, 10,'Hora: '.$Hora.'',0,0,'R');
-$pdf->Cell(15, 10,'Curso: '.$Curso.'',0,0,'R');
-$pdf->Ln(8);
-$pdf->Cell(60,0,'','T');
-$pdf->Ln(0);
-$pdf->Cell(25, 10, 'Prof: '.$Profesor.'', 0);   
-// linea
-$pdf->Ln(8);
-$pdf->Cell(60,0,'','T');
-$pdf->Ln(2);    
  
 // PIE DE PAGINA
 $pdf->Ln(10);
