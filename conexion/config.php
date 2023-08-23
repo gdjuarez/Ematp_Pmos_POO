a80931f2958b02220f4c23107ef4e6044a81c05d
gdjuarez <33164842+gdjuarez@users.noreply.github.com>
2023-08-23 08:11:33 -0300

    Update config.php

--- conexion/config.php
+++ conexion/config.php
@@ -1,17 +1,16 @@
 <?php
- 
-    // constantes de conexion local
-    define('DB_HOST','localhost');   
-    define('DB_USUARIO','root');
-    define('DB_PASS','');  
-    define('DB_NOMBRE','u345632271_pmo'); 
-    define('DB_CHARSET','utf8');
+  // constantes de conexion local
+//   define('DB_HOST','localhost');   
+//   define('DB_USUARIO','c2031804_pmo');
+//   define('DB_PASS','talafiZI36');  
+//   define('DB_NOMBRE','c2031804_pmo'); 
+//   define('DB_CHARSET','utf8');
 
-    // constantes de conexion HOSTINGER
-    // define('DB_HOST','localhost');   
-    // define('DB_USUARIO','u345632271_Gifsys15022019');
-    // define('DB_PASS','Prestamos1512');  
-    // define('DB_NOMBRE','u345632271_pmo'); 
-    // define('DB_CHARSET','utf8');
+//constantes de conexion local
+define('DB_HOST','localhost');   
+define('DB_USUARIO','root');
+define('DB_PASS','');  
+define('DB_NOMBRE','u345632271_pmo'); 
+define('DB_CHARSET','utf8');
 
 ?>
