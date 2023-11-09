29dbed179be714dae58001a40f9a7fc46eb40dea
gdjuarez <33164842+gdjuarez@users.noreply.github.com>
2023-11-09 18:17:05 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+ 
+    // constantes de conexion local
+    define('DB_HOST','localhost');   
+    define('DB_USUARIO','c2031804_pmo');
+    define('DB_PASS','talafiZI36');  
+    define('DB_NOMBRE','c2031804_pmo'); 
+    define('DB_CHARSET','utf8');
+
+  //constantes de conexion local
+  //define('DB_HOST','localhost');   
+  //define('DB_USUARIO','root');
+  //define('DB_PASS','');  
+  //define('DB_NOMBRE','u345632271_pmo'); 
+  //define('DB_CHARSET','utf8');
+
+?>
