c99277e7056be4a427c0a23a329d9742b6773777
Gustavo Juarez <33164842+gdjuarez@users.noreply.github.com>
2024-04-01 18:48:17 -0300

    Update config.php

--- conexion/config.php
+++ conexion/config.php
@@ -1,16 +1,16 @@
 <?php
   // constantes de conexion local
-  define('DB_HOST','localhost');   
-  define('DB_USUARIO','u345632271_gdjnet');
+/*   define('DB_HOST','localhost');   
+  define('DB_USUARIO','u345632271_net');
   define('DB_PASS','151295Lau#');  
   define('DB_NOMBRE','u345632271_net'); 
-  define('DB_CHARSET','utf8');
+  define('DB_CHARSET','utf8'); */
 
 //constantes de conexion local
-/* define('DB_HOST','localhost');   
+ define('DB_HOST','localhost');   
 define('DB_USUARIO','root');
 define('DB_PASS','');  
-define('DB_NOMBRE','u345632271_pmo'); 
+define('DB_NOMBRE','u345632271_net'); 
 define('DB_CHARSET','utf8');
- */
+ 
 ?>
